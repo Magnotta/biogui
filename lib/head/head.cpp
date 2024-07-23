04297882170b3eb78af59be692e1bdc747eaedfd
Magnotta <demariano95@gmail.com>
2024-07-23 12:42:38 -0300

    fixed relay HIGH before pinMode

--- lib/head/head.cpp
+++ lib/head/head.cpp
@@ -20,8 +20,6 @@ LEDHead::LEDHead(uint8_t MOSFET_gate_pin, uint8_t relay_pin,
 	for(uint8_t i = 0; i < 10; i++){
 		_preheat_buf[i] = 0.0;
 	}
-
-	digitalWrite(_relay_pin, HIGH);
 }
 
 void LEDHead::init(){
@@ -30,6 +28,7 @@ void LEDHead::init(){
 	_source_amp_sens.init();
 	pinMode(_MOSFET_gate_pin, OUTPUT);
 	pinMode(_relay_pin, OUTPUT);
+	digitalWrite(_relay_pin, HIGH);
 }
 
 void LEDHead::step(){
